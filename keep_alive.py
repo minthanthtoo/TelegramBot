e0b6275850c432fce608817990f17364227e208d
Min Thant Htoo <minthanthtoo.cs@gmail.com>
2024-10-09 05:08:44 +0630

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 from threading import Thread
 
-app = Flask (_name_)
+app = Flask(_name_)
 
-@app. route('/')
+@app.route('/')
 def index:
   return "Alive"
 
 def run:
-app. run (host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-  t = Thread (target=run)
+  t = Thread(target=run)
